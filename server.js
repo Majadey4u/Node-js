b6c7de39fd1b75999ad9e06cfdd1a5e7bc144772
majadey4u <maryjaneogbuju@gmail.com>
2024-03-29 14:43:10 +0100

    new

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 const http = require('http');
-const fs = require('fs');
+
+const hostname = '127.0.0.1'; // Localhost IP address
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Hello Node!!!!</h1>\n');
-    res.end();
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello Node!!!!</h1>\n');
 });
 
-fs.open('input.txt', 'r+', function(err, fd) {
-    if (err) {
-        return console.error(err);
-    }
-    console.log("File opened successfully!");
-    console.log("Going to read the file");
-    
-    fs.read(fd, buf, 0, buf.length, 10, function(err, bytes/*, buffer*/){
-        if (err){
-            return console.log(err);
-        }
-        console.log(bytes + " bytes read");
-
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://<span class="math-inline">\{hostname\}\:</span>{port}/`);
 });
--- welcome.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile('welcome.txt', 'Hello Node', (err) => {
-    if (err) throw err;
-    console.log('File created successfully');
-});
-
